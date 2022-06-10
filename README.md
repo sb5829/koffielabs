370e357d251dda43cd1f99fd291021c6c8d0d8e6
Sabrena Beck <sb5829@nyu.edu>
2022-06-10 17:33:03 -0400

    Final enhancements

--- README.md
+++ README.md
@@ -11,4 +11,40 @@ I will assume that you have Intellij. The following instructions to create a vir
 If you have a different IDE and need help setting one up, please contact me.
 
 * Open Intellij. File > New > Project From Version Control 
-* Enter the following under url: 
+* Enter the following under url: https://github.com/sb5829/koffielabs.git
+* Open in New Window
+* File > Project Structure >SDKs
+* Add Python SDK > Pipenv Environment (choose your python executable)
+* File > Project Structure > Project > Select new Pipenv as SDK
+* File > Project Structure > Module > Fastapi 
+* Mark KoffieProject as root source
+* From Terminal and at project root KoffieProject, run pipenv shell (if you do not have pipenv, please run pip install pipenv)
+
+## Install dependencies 
+
+Install the dependencies by running the following command under KoffieProject directory:
+pipenv install -v --dev --ignore-pipfile
+
+The flag --ignore-pipfile forces to use the Pipfile.lock and all pinned version from there
+
+## Create Fastapi running configuration 
+* At the top right next to the green triangle, please click on Add Configuration
+* Please select FastApi
+* Application file should be: <your_root_path_where_project_is_located>/KoffieProject/main.py
+* On the uvicorn command paste: --reload
+* For python interpreter, please select the project's SDK 
+
+## Run the application
+Universe willing, now you should be able to press that play green triangle and the application will run on http://127.0.0.1:8000!
+
+There are two ways you can utilize the APIs created:
+
+* Directly search into your browser the desired route, i.e. http://127.0.0.1:8000/export/
+* Go to the FastApi docs UI http://127.0.0.1:8000/docs#/ and interact with the APIs there
+* Enter the request data as defined in the FastApi docs
+* IMPORTANT: if it's your first time running, please call the api "create_table" to create the database
+
+## Questions/Concerns
+
+Please let me know if there are any questions/concerns by reaching out to me at sb5829@nyu.edu
+Again, thank you for taking the time to look at my challenge, and I look forward to hearing back!
--- main.py
+++ main.py
@@ -3,15 +3,16 @@ from typing import Union
 import logging
 
 import os
-from fastapi import FastAPI
+from fastapi import FastAPI, Query
 import requests
 import pandas as pd
 import pyarrow as pa
 import pyarrow.parquet as pq
-from fastapi.responses import FileResponse
+from fastapi.responses import FileResponse, Response
 
 app = FastAPI()
 
+# check same thread disabled in order to run multiple sql executions
 conn = sqlite3.connect('koffie.db', check_same_thread=False)
 logging.getLogger().setLevel(logging.INFO)
 
@@ -38,10 +39,12 @@ def insert_to_table(vin, cache):
 
 
 def decode_vin(vin):
+    # call the vPIC api in order to decode the given VIN
     url = f'https://vpic.nhtsa.dot.gov/api/vehicles/DecodeVinValues/{vin}?format=json'
     try:
         r = requests.get(url)
         json_response = r.json()
+        # return as json response
         return json_response
     except Exception as e:
         logging.error(f"Unable to get response from vPIC API with exception {e}")
@@ -49,15 +52,17 @@ def decode_vin(vin):
 
 
 def retrieve_data():
+    # select all the entries in the database and return them
     sql = """SELECT * FROM VIN_STORAGE;"""
     cur = conn.cursor()
     cur.execute(sql)
     results = cur.fetchall()
-    print(results)
+    logging.info(f"The results from retrieving the data for cached VINs are: {results}")
     return results
 
 
 def store_results(response):
+    # check that there has been a valid response with results given from API
     if response and response.get('Results'):
         results = response['Results'][0]
         vin = results.get('VIN')
@@ -70,9 +75,9 @@ def store_results(response):
         cur.execute(sql, (vin, make, model, model_year, body_class))
         logging.info(f"Entry for VIN {vin} was entered")
         return [make, model, model_year, body_class, vin]
-
     else:
         logging.error(f"Response was not available. Response message is: {response.get('message')}")
+        return None
 
 
 def vin_exists(vin):
@@ -83,18 +88,21 @@ def vin_exists(vin):
     res = cur.fetchone()
     if res:
         print("Found!")
+        logging.info(f"Entry for VIN {vin} was found in cache, return cached results")
+        # return False as the vin entry was not cached
         return False, res
     # the entry does not exist, add it to the database
     else:
         logging.info(f"Entry for VIN {vin} was not found in cache, decoding VIN now")
         result = decode_vin(vin)
         res = store_results(result)
-        print("Not found...")
+        # return True as VIN has been cached
         return True, res
 
 
 def remove_vin(vin):
     cur = conn.cursor()
+    # search if VIN is already cached in database
     vin_exist, res = vin_exists(vin)
     if not vin_exist:
         sql = """DELETE FROM VIN_STORAGE WHERE VIN=?"""
@@ -107,22 +115,27 @@ def remove_vin(vin):
 
 
 def export_file():
+    # get cached data
     result = retrieve_data()
     data = {}
+    # create a column number for each of the entries
     for res in result:
         data['col'+str(res[0])] = [res[1], res[2], res[3], res[4], res[5]]
+    # index will always be length of 5 because of the parameters already defined as make, model, year and body class
+    # create data frame with data and number of items in each colum
     df = pd.DataFrame(data=data, index=[0, 1, 2, 3, 4])
+    # convert dataframe into a table
     table = pa.Table.from_pandas(df)
+    # use the table to write into parquet format
     file = pq.write_table(table, 'export_vin_cache.parquet')
-    print("read parquet")
-    read = pd.read_parquet('export_vin_cache.parquet')
-    print(read)
-    print("done reading")
     return file
 
 
 @app.get("/lookup/")
-def lookup(vin: str):
+# vin definition makes it required and makes sure that it is of length 17
+def lookup(vin: str = Query(min_length=17, max_length=17)):
+    # check if vin already exists within the database. If no records are found, vin exists will create entry, if found
+    # vin_exists will query the results and return them
     exists, res = vin_exists(vin)
     try:
         if not exists:
@@ -136,7 +149,7 @@ def lookup(vin: str):
 
 
 @app.get("/remove/")
-def read_item(vin: str):
+def read_item(vin: str= Query(min_length=17, max_length=17)):
     res = remove_vin(vin)
     return {"vin": vin, "cache_delete_success": res}
 
@@ -146,16 +159,10 @@ def read_item():
     export_file()
     root_dir = os.path.dirname(os.path.abspath(__file__))
     file_path = os.path.join(root_dir, 'export_vin_cache.parquet')
-
     return FileResponse(file_path)
 
-"""
-res = decode_vin('4V4NC9EJXEN171694')
-store_results(res)
-res = decode_vin('1XP5DB9X7YN526158')
-store_results(res)
-retrieve_data()
-vin_exists('1XP5DB9X7YN526158')
-vin_exists('1XP5DB9X7YN52618')
-"""
 
+@app.get("/create_table")
+def create_initial_table():
+    create_table()
+    return "Table created"
